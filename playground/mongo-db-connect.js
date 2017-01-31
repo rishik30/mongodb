76fad973a636fa4f22064567321036f03c484827
Rishabh Kumar <rishabhk3003@gmail.com>
2017-01-31 15:20:25 +0530

    initial commit

---
+++ playground/mongo-db-connect.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err) return console.log('Unable to connect to mongo db server');
+    console.log('Connected to Mongo db server');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something',
+    //     completed: false
+    // }, (err, result) => {
+    //     if(err) return console.log('Unable to insert todo', err)
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    db.collection('Users').insertOne({
+        name: 'John D',
+        age: 30,
+        location: 'Paris'
+    }, (err, result) => {
+        if(err) return console.log('Unable to insert the user', err)
+        console.log(JSON.stringify(result.ops, undefined, 2))
+    })
+
+    db.close();
+});
---
+++ playground/mongo-db-find.js
@@ -0,0 +1,17 @@
+// const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err) return console.log('Unable to connect to mongo db server');
+    console.log('Connected to Mongo db server');
+
+    db.collection('Todos').find().toArray().then((docs) => {
+        console.log('Todos')
+        console.log(JSON.stringify(docs, undefined, 2))
+    })
+    .catch((err) => {
+
+    })
+
+    db.close();
+});
